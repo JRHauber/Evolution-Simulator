f989ce5607d3d58d540f2d1fd815d2d91480dd6c
Jonathan Hauber <jrhauber@gmail.com>
2021-11-21 03:04:09 -0600

    Add project files.

---
+++ Classes/Creature.py
@@ -0,0 +1,4 @@
+class creature(object):
+    
+
+
---
+++ Classes/Genome.py
@@ -0,0 +1,5 @@
+class class1:
+    def __init__(genome, brain, loc):
+        this.genome = genome
+        this.brain = brain
+        this.loc = loc
